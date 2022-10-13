2b19b8a0fd9e5e7b81b79bdc85c9998e4aa01ec0
User <User@DESKTOP-M6J9EGF>
2022-10-12 21:15:07 -0600

    Agregar archivos de proyecto.

---
+++ L10+SBLG+1104822/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L10_SBLG_1104822
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int cantidad = 0;
+            int contador = 0;
+            string usuario;
+            string contraseña;
+            bool res = false;
+
+
+            while (cantidad < 3)
+            {
+                Console.WriteLine("Ingrese el usuario");
+                usuario = Console.ReadLine();
+                Console.WriteLine("Coloque contraseña");
+                contraseña = Console.ReadLine();
+
+                res = login(usuario, contraseña);
+                if (res == true)
+                {
+                    cantidad = 3;
+                    Console.WriteLine("Ingreso accedido");
+                }
+                else
+                {
+                    Console.WriteLine("Error al ingresar, ingrese de nuevo, cantidad de intentos: " + contador);
+                    contador++;
+                    cantidad++;
+                }
+                if (contador == 3)
+                {
+                    Console.WriteLine("Se han agotado las oportunidades");
+                }
+
+            }
+
+            Console.ReadKey();
+        }
+        public static bool login(string usuario, string contra)
+        {
+            if (usuario == "usuario1" && contra == "asdasd")
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+
+            }
+        }
+    }
+}
